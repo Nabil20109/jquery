8886e3eebd606ae245f3a2b45dcca77fbc156d7f
Nabil20109 <mahmudnabil710@gmail.com>
2025-02-19 03:29:13 -0800

    file

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
-        $(".two").click.function(){
-            alert("Button Clicked")
-        }
+        $(".two").click(function(){
+            alert("Button clicked")
+        });
 	
 });
