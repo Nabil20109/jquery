ff5fefba8348a33598d95242f32e30497207b8b3
Nabil20109 <mahmudnabil710@gmail.com>
2025-02-19 07:41:05 -0800

    custo fil change

--- custom.js
+++ custom.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-    
-        $(".two").click(function(){
-            alert("Hello ")
-        });
-	
+
+    $(".two").click(function(){
+        $(".one").hide(2000);
+        
+
+    });
+    $(".btn").click(function(){
+        $(".one").show(2000);
+       
+       
+    });
+        
 });
