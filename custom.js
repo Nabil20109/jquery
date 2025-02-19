89516d7042498596e25386cfa04dba242f7fe81c
Nabil20109 <mahmudnabil710@gmail.com>
2025-02-19 02:56:33 -0800

    test

--- custom.js
+++ custom.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
-		let newhtml = "<strong>Hi I Am Nabil Mahmud</strong> <br> <h1>Hi I Am Samin Yasar</h1>"   
-        $(".one").html(newhtml)
-
+		
         
 	
 });
